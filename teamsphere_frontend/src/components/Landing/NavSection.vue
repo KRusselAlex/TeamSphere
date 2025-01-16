2e8e6730f55cfaa95529d0c3a980457a24769af2
KAlexRussel <kouawoualex1234@gmail.com>
2025-01-16 13:18:54 +0100

    [Update]: update navbar padding

--- teamsphere_frontend/src/components/Landing/NavSection.vue
+++ teamsphere_frontend/src/components/Landing/NavSection.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import LogoImage from '@/components/LogoImage.vue';
+import { ref } from 'vue';
+import { logoutUser } from '@/api/auth/authServices';
+import { useRouter } from 'vue-router';
+
+
+const router = useRouter();
+const user = ref(localStorage.getItem('userToken'))
+
+const handleLogout = async () => {
+  await logoutUser()
+  router.push("/auth/login")
+}
+
 </script>
 <template>
-  <header class="flex justify-between items-center  bg-primaryColor py-8 px-16">
+  <header class="flex justify-between items-center  bg-primaryColor py-4 px-16">
 
     <RouterLink to="/" class="flex items-center gap-3">
-      <LogoImage color="#fff" size="60" />
+      <LogoImage color="#fff" size="40" />
       <div class="flex items-center text-3xl text-white font-sans font-bold">
         TeamSphere
       </div>
@@ -20,9 +33,9 @@ import LogoImage from '@/components/LogoImage.vue';
 
       <div class="hidden md:flex justify-end items-center gap-x-3">
 
-        <RouterLink to="/auth/register">
+        <RouterLink to="/auth/register"  v-if="!user">
           <button type="button"
-            class=" py-3 px-4 font-semibold text-gray-800 bg-[#fff]  font-sans  rounded-full  hover:bg-thirdColor hover:text-white   ">
+            class=" py-2 px-4 font-semibold text-gray-800 bg-[#fff]  font-sans  rounded-full  hover:bg-thirdColor hover:text-white   ">
 
             Rejoignez Nous
           </button>
@@ -30,13 +43,13 @@ import LogoImage from '@/components/LogoImage.vue';
         <div>
 
           <button type="button"
-            class=" py-3 px-4 font-semibold  bg-white font-sans  rounded-full hover:bg-thirdColor  text-gray-800 "
-            v-if="userpp" @click="handleLogout">
+            class=" py-2 px-4 font-semibold  bg-white font-sans  rounded-full hover:bg-thirdColor  text-gray-800 "
+            v-if="user" @click="handleLogout">
             Deconnexion
           </button>
 
           <RouterLink to="/auth/login" v-else>
-            <button type="button" class=" py-3 px-4 font-semibold  bg-white font-sans  rounded-full text-gray-800 ">
+            <button type="button" class=" py-2 px-4 font-semibold  bg-white font-sans  rounded-full text-gray-800 ">
               Connexion
             </button>
 
@@ -44,8 +57,8 @@ import LogoImage from '@/components/LogoImage.vue';
 
         </div>
 
-        <a href="/dashboard" v-if="userpp">
-          <button type="button" class=" py-3 px-4 font-semibold  bg-white font-sans  rounded-full text-gray-800 ">
+        <a href="/dashboard" v-if="user">
+          <button type="button" class=" py-2 px-4 font-semibold  bg-white font-sans  rounded-full text-gray-800 ">
             Votre Tableau
           </button>
         </a>
@@ -57,9 +70,4 @@ import LogoImage from '@/components/LogoImage.vue';
   </header>
 </template>
 
-    <script setup lang="ts">
-
-
-
-    </script>
 
